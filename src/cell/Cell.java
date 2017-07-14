ffce96bfdfdf747340eae375a119a05644995aa2
kataji <kataji@users.noreply.github.com>
2017-07-14 14:23:00 +0800

    modified cellmachine to foxnrabbit

--- src/cell/Cell.java
+++ src/cell/Cell.java
@@ -2,25 +2,6 @@ package cell;
 
 import java.awt.Graphics;
 
-public class Cell {
-	private boolean alive = false; //一开始最好给个初值？虽然自动也会给来着
-	
-	public void die(){
-		alive = false;
-	}
-	
-	public void reborn(){
-		alive = true;
-	}
-	
-	public boolean isAlive(){
-		return alive;
-	}
-	
-	public void draw(Graphics g, int x, int y, int size){
-		g.drawRect(x, y, size, size);
-		if(alive){
-			g.fillRect(x, y, size, size);
-		}
-	}
+public interface Cell {
+	void draw(Graphics graphic, int x, int y, int size);
 }
